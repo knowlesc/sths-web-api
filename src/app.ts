9d62f387aa09f6d4bd1a5fa10478788674419eb5
knowlesc <knowlesc8@gmail.com>
2017-04-29 13:31:25 -0700

    Add status message route

--- src/app.ts
+++ src/app.ts
@@ -10,6 +10,10 @@ const log = new Logger('App');
 
 const app = express();
 
+app.get('/', (req, res) => {
+  res.send('API is running.');
+});
+
 app.use((req, res, next) => {
   res.header('Access-Control-Allow-Origin', '*');
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
