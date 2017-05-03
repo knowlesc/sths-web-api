ec51ad0d5f1e3a942ae8f40749012db7ef99afec
knowlesc <knowlesc8@gmail.com>
2017-05-02 19:36:13 -0700

    Expose X-Total-Count header

--- src/app.ts
+++ src/app.ts
@@ -17,6 +17,7 @@ app.get('/', (req, res) => {
 app.use((req, res, next) => {
   res.header('Access-Control-Allow-Origin', '*');
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  res.header('Access-Control-Expose-Headers', 'X-Total-Count');
   next();
 });
 
