1ca098a8ec099b990c8d76ef5646ab6371a6bb7d
knowlesc <knowlesc8@gmail.com>
2017-09-13 17:30:50 -0700

    Always list dmen as D

--- src/services/players/getSkaterInfo/getSkaterInfo.queries.ts
+++ src/services/players/getSkaterInfo/getSkaterInfo.queries.ts
@@ -1,6 +1,7 @@
 export class GetSkaterInfoQueries {
   // SELECT
   static positionFormula = `CASE
+    WHEN PlayerInfo.PosD = 'True' THEN 'D'
     WHEN PlayerInfo.PosC = 'False'  AND PlayerInfo.PosLW = 'False'  AND PlayerInfo.PosRW = 'False'  THEN 'D'
     WHEN PlayerInfo.PosC = 'False'  AND PlayerInfo.PosLW = 'False'  AND PlayerInfo.PosRW = 'True'   THEN 'RW'
     WHEN PlayerInfo.PosC = 'False'  AND PlayerInfo.PosLW = 'True'   AND PlayerInfo.PosRW = 'False'  THEN 'LW'
@@ -9,7 +10,6 @@ export class GetSkaterInfoQueries {
     WHEN PlayerInfo.PosC = 'True'   AND PlayerInfo.PosLW = 'False'  AND PlayerInfo.PosRW = 'True'   THEN 'C/RW'
     WHEN PlayerInfo.PosC = 'True'   AND PlayerInfo.PosLW = 'True'   AND PlayerInfo.PosRW = 'False'  THEN 'C/LW'
     WHEN PlayerInfo.PosC = 'True'   AND PlayerInfo.PosLW = 'True'   AND PlayerInfo.PosRW = 'True'   THEN 'C/LW/RW'
-    WHEN PlayerInfo.PosD = 'True' THEN 'D'
   END`;
 
   static teamAbbreFormula = `CASE
